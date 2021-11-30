7fc129e2b92a6d2d71301d3359ac7e3c1ae741c6
Kristian K <kristianklos@gmail.com>
2021-11-30 11:03:58 +0100

    first commit

---
+++ src/components/DemoRouter.js
@@ -0,0 +1,64 @@
+import React, { Fragment, useState } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link, useHistory, useParams, useLocation, Redirect } from "react-router-dom";
+
+
+
+const DemoRouter = () => {
+    return (
+        <div className="container">
+            <Router>
+                <Header />
+                <Switch>
+                    <Route exact path="/" component={Welcome} />
+                    <Route path="/home" component={Home}  />
+                    <Route path="/person" component={Person}  />
+                    <Route path="/about" component={About}  />
+
+                    <Route component={PageNotFound} />
+
+                </Switch>
+            </Router>
+        </div>
+    )
+}
+
+const Header = () => {
+    return(
+        <ul className="nav nav-pills nav-fill bg-dark text-white">
+            <li className="nav-item">
+                <Link className="nav-link" to={Welcome} >Welcome</Link>
+            </li>
+            <li className="nav-item">
+                <Link className="nav-link" to={Home} >Home</Link>
+            </li>
+            <li className="nav-item">
+                <Link className="nav-link" to={Person}>Person</Link>
+            </li>
+            <li className="nav-item">
+                <Link className="nav-link" to={About} >About</Link>
+            </li>
+        </ul>
+    )
+};
+
+const Welcome = () => {
+    return (<b>Welcome</b>)
+}
+
+const Home = () => {
+    return (<b>Home</b>)
+}
+
+const About = () => {
+    return (<b>About us</b>)
+}
+
+const Person = () => {
+    return (<b>Person</b>)
+}
+
+const PageNotFound = () => {
+    return (<b>404: Page not found</b>)
+}
+
+export default DemoRouter;
